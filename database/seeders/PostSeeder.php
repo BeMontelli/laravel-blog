20832eae0acb157113d3a070d97a6f9bd37f3839
benjaminmontellimard <benjamin.m@coqpit.fr>
2024-04-08 17:13:27 +0200

    Added Model + Seeder Posts

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('posts')->insert([
+            'title' => Str::random(10),
+            'description' => 'description',
+            'content' => 'content',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+    }
+}
