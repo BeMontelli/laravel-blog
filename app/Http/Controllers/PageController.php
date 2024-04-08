13d765a1fb3100111a64146f63157c59bc9f6181
benjaminmontellimard <benjamin.m@coqpit.fr>
2024-04-08 13:16:03 +0200

    Added variables views

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,10 +10,16 @@ class PageController extends Controller
     //
     public function legals(): View
     {
-        return view('legals');
+        return view('legals', [
+            'title' => 'Legals',
+            'content' => 'Lorem Ipsum',
+        ]);
     }
     public function aboutus(): View
     {
-        return view('aboutus');
+        return view('aboutus', [
+            'title' => 'About Us',
+            'content' => '<p>Lorem Ipsum ...</p><p>Lorem Ipsum 2 ...</p>',
+        ]);
     }
 }
--- resources/views/legals.blade.php
+++ resources/views/legals.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Legals</title>
+        <title>{{ $title }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -17,15 +17,7 @@
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget turpis eu augue faucibus commodo. Nullam at eleifend lacus. Aliquam sit amet viverra elit. Cras egestas a magna sed fermentum. Proin vel egestas turpis, in scelerisque lectus. Morbi finibus, turpis a finibus congue, leo risus aliquam enim, at feugiat felis sapien at quam. Nunc convallis pellentesque arcu et cursus. Vivamus vestibulum efficitur velit, a volutpat purus accumsan in. Cras commodo tristique risus.
-
-    Etiam tellus felis, interdum ac pharetra id, efficitur tempus nisl. Fusce ut felis porta, scelerisque magna ac, ultricies dolor. Donec et faucibus nibh, eu malesuada purus. Nulla posuere facilisis convallis. Aenean sit amet justo mi. Sed velit felis, pulvinar quis metus a, auctor sodales lacus. Nullam gravida euismod purus ac ultrices.
-
-    Praesent malesuada maximus metus, gravida laoreet augue tristique eu. Pellentesque condimentum vestibulum nisl, et volutpat leo lobortis quis. Nam non sapien massa. Nulla facilisi. Etiam tempus condimentum lobortis. Sed et dolor lorem. Nam mauris ex, euismod sit amet lacus venenatis, tristique efficitur ante. Mauris tempor pulvinar nibh, at bibendum dui lobortis ut. Nam porttitor, mi eget tincidunt ultricies, nibh purus lacinia risus, vel venenatis purus ante in ex. Nam nec nisl interdum, maximus tortor hendrerit, cursus justo. Aliquam erat volutpat. Morbi a pulvinar sem. In euismod neque dapibus lacus commodo interdum. Morbi et consequat lacus, non molestie ligula.
-
-    Proin mollis erat ex, quis sodales massa dapibus eget. Integer euismod erat posuere volutpat pretium. Aenean at hendrerit dui, a consectetur metus. Maecenas ut interdum urna, in malesuada elit. Sed condimentum dignissim felis, sed ultricies nunc mollis ut. Etiam at augue risus. Nunc at enim ornare, cursus sapien sed, pretium mi. Nam mattis luctus nibh at maximus. Pellentesque eu tellus mattis, iaculis risus imperdiet, euismod neque.
-
-    Suspendisse hendrerit lobortis scelerisque. Phasellus a orci dui. Ut id tortor eu enim tempus condimentum nec non erat. Phasellus fermentum lectus sit amet pretium posuere. Integer non feugiat nisi. Maecenas ultricies condimentum metus et laoreet. Proin porta convallis metus sit amet molestie. Pellentesque eget consectetur est. Vestibulum nec massa vel leo egestas ornare et sed magna. Vivamus ac risus nulla.
+    {!! $content !!}
 
     </body>
 </html>
