d44bfd9baffec8ab3958026631e188f978fe8d9c
benjaminmontellimard <benjamin.m@coqpit.fr>
2024-04-17 11:46:01 +0200

    Added categories page blog

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use Illuminate\Support\Facades\Auth;
@@ -12,10 +13,25 @@ class PostController extends Controller
     public function index(): View
     {
         $posts = Post::all();
+        $categories = Category::all();
 
         return view('blog', [
             'title' => 'My posts',
             'content' => '<h1>My posts</h1><p>Lorem Ipsum ...</p>',
+            'categories' => $categories,
+            'posts' => $posts,
+        ]);
+    }
+
+    public function category($id) : View
+    {
+        $category = Category::find($id);
+        $categories = Category::all();
+        $posts = $category->posts;
+        return view('blogcategory', [
+            'title' => $category->title,
+            'content' => $category->description,
+            'categories' => $categories,
             'posts' => $posts,
         ]);
     }
