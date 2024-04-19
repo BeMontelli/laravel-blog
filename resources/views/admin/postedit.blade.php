4efe2ce368b410dc4955f222a834c77e82c4f211
benjaminmontellimard <benjamin.m@coqpit.fr>
2024-04-19 09:53:03 +0200

    fix styles postedit.blade.php

--- resources/views/admin/postedit.blade.php
+++ resources/views/admin/postedit.blade.php
@@ -9,44 +9,63 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
                     <form action="{{ route('admin.posts.update', $post->id) }}" method="post" enctype="multipart/form-data">
                         @csrf
                         @method("PUT")
-                        <div class="form-group">
-                            <label for="title">Title</label>
-                            <input value="{{ $post->title }}" type="text" class="text-black form-control" id="title" name="title" required>
+                        <div class="">
+                            <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" for="title">
+                                Title
+                            </label>
+                            <input class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full" id="title" name="title" type="text" value="{{ $post->title }}" required="required" autofocus="autofocus" autocomplete="title">
                         </div>
-                        <div class="form-group">
-                            <label for="body">description</label>
-                            <textarea class="text-black form-control" id="description" name="description" rows="3" required>{{ $post->description }}</textarea>
+                        <div class="mt-6">
+                            <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" for="description">
+                                Description
+                            </label>
+                            <input class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full" id="description" name="description" type="text" value="{{ $post->description }}" required="required" autofocus="autofocus" autocomplete="description">
                         </div>
-                        <div class="form-group">
-                            <label for="body">content</label>
-                            <textarea class="text-black form-control" id="content" name="content" rows="3" required>{{ $post->content }}</textarea>
+                        <div class="mt-6">
+                            <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" for="content">
+                                Content
+                            </label>
+                            <textarea class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full" id="content" name="content" required="required" autofocus="autofocus">{{ $post->content }}</textarea>
                         </div>
-                        <div class="form-group">
+
+                        <div class="form-group mt-6">
                             <label class="form-label" for="imageinput">Image:</label>
                             <input
                                 type="file"
                                 name="imageinput"
                                 id="imageinput"
                                 class="form-control @error('image') is-invalid @enderror">
-                                <img style="max-width: 400px" src="{{ URL::to('/')}}/{{$post->image }}" alt="{{ $post->title }}">
+                            <img style="max-width: 400px" src="{{ URL::to('/')}}/{{$post->image }}" alt="{{ $post->title }}">
                         </div>
-                        <br>
-                        <fieldset class="mb-6 text-white">
-                            <legend>Categories</legend>
+
+                        <fieldset class="mt-6 text-white">
+                            <legend class="mb-2 block font-medium text-sm text-gray-700 dark:text-gray-300">Categories</legend>
                             @if (!empty($categories) && count($categories) > 0)
-                                @foreach ($categories as $category)
-                                    <div>
-                                        <input type="checkbox" id="cat-{{ $category->id }}" name="categories[]" value="{{ $category->id }}" @if (in_array($category->id, $idCategories)) checked @endif />
-                                        <label for="cat-{{ $category->id }}">{{ $category->title }}</label>
-                                    </div>
-                                @endforeach
+                                <div class="checkboxes flex inline-flex">
+                                    @foreach ($categories as $category)
+                                        <div class="p-2" style="width: fit-content;">
+                                            <input type="checkbox" id="cat-{{ $category->id }}" name="categories[]" value="{{ $category->id }}" @if (in_array($category->id, $idCategories)) checked @endif />
+                                            <label for="cat-{{ $category->id }}">{{ $category->title }}</label>
+                                        </div>
+                                    @endforeach
+                                </div>
                             @endif
                         </fieldset>
                         <br>
-                        <button type="submit" class="btn btn-primary">Update Post</button>
+                        <button type="submit" class="btn btn-primary btn__classic">Update Post</button>
                     </form>
                 </div>
             </div>
